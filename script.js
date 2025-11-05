1d58841e82fce41399689b984f127e933195c693
Mario Acevedo Aguilar <marioacevedoaguilar@Marios-MacBook-Pro.local>
2025-11-05 10:40:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('cta').addEventListener('click', ()=> {
+  alert('¡Listo! Ahora integra tu diseño de Figma en esta estructura.');
+});
